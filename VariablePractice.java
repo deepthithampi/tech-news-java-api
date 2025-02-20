d10fdfb0b5722522710be249cecd020a2b0ce67d
Deepthi Sandeep <deepththampi@hotmail.com>
2025-02-20 11:17:21 -0600

    Added the entity classes

---
+++ VariablePractice.java
@@ -0,0 +1,13 @@
+public class VariablePractice {
+    public static void main(String[] args) {
+        int x = 5;
+        double d = 3.14;
+        String s = "Hello";
+        double sum = x + d;
+
+        String newString = s +x ;
+        System.out.println("Sum : "+sum);
+        System.out.println("Concat : "+newString);
+
+    }
+}
---
+++ VariableTypes.java
@@ -0,0 +1,9 @@
+public class VariableTypes {
+    public static void main(String[] args) {
+        int x = 5;
+        String s = "Hello World";
+        System.out.println("Number : "+x);
+        System.out.println("String : "+s);
+
+    }
+}
